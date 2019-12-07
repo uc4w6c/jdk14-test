6d3367a5eeb45eab8fed1e3c582a9e2a9652f3bb
Yuta <uc4w6c@bma.biglobe.ne.jp>
2019-12-07 12:26:39 +0900

    CompactNumberFormat

---
+++ src/com/example/CompactNumberFormat.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import java.text.NumberFormat;
+
+class CompactNumberFormat {
+    public static void main(String[] args) {
+	NumberFormat cnf = NumberFormat.getCompactNumberInstance();
+	System.out.println(cnf.format(10000));
+	System.out.println(cnf.format(10000_0000));
+	System.out.println(cnf.format(10000_0000_0000L));
+	System.out.println(cnf.format(10000_0000_0000_0000L));
+    }
+}
