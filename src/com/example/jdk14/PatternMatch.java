e2fdd11dea485c0a0545891893b0f7f5c59fc3f5
Yuta <uc4w6c@bma.biglobe.ne.jp>
2019-12-13 00:31:39 +0900

    PatternMatchを修正

--- src/com/example/jdk14/PatternMatch.java
+++ src/com/example/jdk14/PatternMatch.java
@@ -3,9 +3,16 @@ package com.example.jdk14;
 class PatternMatch {
     public static void main(String[] args) {
 	Object o = "Pattern Match of instanceof";
+	// if (o instanceof String s) {
+	//     System.out.println(s.length());
+	// }
 	if (o instanceof String) {
-	    String s = o;
-	    System.out.println(s);
+	    String s = (String)o;
+	    System.out.println(s.length());
+	}
+	switch (o) {
+	    case Integer i -> System.out.println(i); break;
+	    case String s -> System.out.println(s.length()); break;
 	}
     }
 }
